1b6e63bb0d92f0ecfcd5291d74c8eafe89d92c6e
wessajosef-sketch <wesajosef@gmail.com>
2026-06-12 20:01:41 +0300

    Update settings.py

--- NJR_project/settings.py
+++ NJR_project/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'NJR_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'notjustrecycling',   # اسم قاعدة البيانات عندك
-        'USER': 'root',               # اسم المستخدم
-        'PASSWORD': 'password',       # الباسورد
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
