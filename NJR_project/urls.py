a40e870c2985d77693af98129048ab09cf6922d4
wessajosef-sketch <wesajosef@gmail.com>
2026-06-12 19:37:54 +0300

    Update urls.py

--- NJR_project/urls.py
+++ NJR_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,3 +18,9 @@ urlpatterns = [
     path('booking/', include('booking.urls')),
     path("", include("core.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
